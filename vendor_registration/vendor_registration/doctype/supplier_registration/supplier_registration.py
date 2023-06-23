474e8109aa695bc375fd9eaee2ba9db7ebe7fadd
Deepak Kumar <deepakkumar@8848digital.com>
2023-06-23 10:16:55 +0530

    create supplier when supplier registration is approved

--- vendor_registration/vendor_registration/doctype/supplier_registration/supplier_registration.py
+++ vendor_registration/vendor_registration/doctype/supplier_registration/supplier_registration.py
@@ -52,6 +52,40 @@ class SupplierRegistration(TransactionBase):
 
 		self.create_primary_contact()
 		self.create_primary_address()
+		# after approved get mapped doc and create supplier
+		if self.get_doc_before_save() and (self.workflow_state != self.get_doc_before_save().workflow_state and self.workflow_state == "Approved"):
+			# create supplier map the field value from Supplier Mapping
+			map = frappe.get_doc("Supplier Mapping").supplier_field_mapping
+			new_supplier = frappe.new_doc("Supplier")
+			# self has supplier registration field
+			for i in map:
+				new_supplier.__dict__[i.doc_field] = self.__dict__[i.registration_field]
+			new_supplier.save()
+			# update new supplier name to supplier registration
+			frappe.db.sql(f'update `tabSupplier Registration` set supplier_created="{new_supplier.name}" where name="{self.name}"')
+			frappe.db.commit()
+			## create Address and link address
+			# addr = frappe.new_doc('Address')
+			# addr.address_title = self.get('address_title')
+			# addr.address_type = self.get('address_type')
+			# addr.address_line1 = self.get('address_line1')
+			# addr.address_line2 = self.get('address_line2')
+			# addr.city = self.get('city')
+			# addr.state = self.get('state')
+			# addr.country = self.get('country')
+			# addr.pincode = self.get('postal_code')
+			# addr.email_id = self.get('email_id')
+			# addr.phone = self.get('mobile_no')
+			# # link address
+			# addr.append('links',{
+			# 	'link_doctype': "Supplier",
+			# 	'link_name': new_supplier.name
+			# })
+			# addr.append('links',{
+			# 	'link_doctype': "Supplier Registration",
+			# 	'link_name': self.name
+			# })
+			# addr.save()
 
 	def validate(self):
 		self.flags.is_new_doc = self.is_new()
